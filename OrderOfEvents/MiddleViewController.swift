a8c9395b1bf28c10c7d4fbeb05bb1d92dadc7d84
JohnPGreen <johnpgreen60@gmail.com>
2018-11-19 23:48:24 -0500

    completed

--- OrderOfEvents/MiddleViewController.swift
+++ OrderOfEvents/MiddleViewController.swift
@@ -9,22 +9,49 @@
 import UIKit
 
 class MiddleViewController: UIViewController {
+        
+    @IBOutlet weak var label: UILabel!
+    
+    var eventNumber = 1
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        if let existingText = label.text {
+            label.text = "\(existingText)\nEvent number \(eventNumber) was viewDidLoad"
+            eventNumber += 1
+        }
+    }
+        
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        if let existingText = label.text {
+            label.text = "\(existingText)\nEvent number \(eventNumber) was viewWillAppear"
+            eventNumber += 1
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        if let existingText = label.text {
+            label.text = "\(existingText)\nEvent number \(eventNumber) was viewDidAppear"
+            eventNumber += 1
+        }
     }
-    */
-
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        if let existingText = label.text {
+            label.text = "\(existingText)\nEvent number \(eventNumber) was viewWillDisappear"
+            eventNumber += 1
+        }
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        if let existingText = label.text {
+            label.text = "\(existingText)\nEvent number \(eventNumber) was viewDidDisappear"
+            eventNumber += 1
+        }
+    }
+    
 }
